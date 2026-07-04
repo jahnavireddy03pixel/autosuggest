a5bfeccccf06266ca5865f710c6a2bbd014a9790
jahnavireddy03pixel <24wh1a05z0@bvrithyderabad.edu.in>
2026-07-04 10:11:32 +0530

    randomuser

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,4 @@
-Index.js : 
 
-```
 const users = [
   {
     "name" :"Jane Doe",
@@ -23,7 +21,23 @@ function toggle(){
   document.getElementById("img").src=users[curUserId].image;
   // name
   document.getElementById("card-name").innerText=users[curUserId].name;
+  document.getElementById("card-gender").innerText=users[curUserId].gender;
+}
+function getRandomUser(){
+  fetch("https://randomuser.me/api/")
+  .then(function(data){
+    return data.json();
+  })
+  .then(function(parsedData){
+    let gender=parsedData.results[0].gender;
+    let first=parsedData.results[0].name.first;
+    let last=parsedData.results[0].name.last;
+    let name=first+" "+last;
+    let imageurl=parsedData.results[0].picture.large;
+    document.getElementById("card-gender").innerText=gender;
+    document.getElementById("card-name").innerText=name;
+    document.getElementById("img").src=imageurl;
+  })
 }
 
 console.log("Hello from JS");
-```
